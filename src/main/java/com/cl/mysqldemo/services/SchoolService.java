6e3a4463d0dc9b186c8aea1da454dc591f9b447c
salwa-said <salwwa2000@gmail.com>
2026-08-31 12:10:00 +0400

    add Service: SchoolService.java

---
+++ src/main/java/com/cl/mysqldemo/services/SchoolService.java
@@ -0,0 +1,29 @@
+package com.cl.mysqldemo.services;
+
+import com.cl.mysqldemo.entities.School;
+import com.cl.mysqldemo.repositories.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class SchoolService {
+    SchoolRepository schoolRepository;
+    @Autowired
+    public SchoolService(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    public Long createSchool(String name,String location){
+        School school= new School();
+        school.setIsActive(true);
+        school.setCreatedDate(new Date());
+        school.setName(name);
+        school.setLocation(location);
+
+        school= schoolRepository.save(school);
+
+        return school.getId();
+    }
+}
