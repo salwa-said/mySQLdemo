c64d4d0cf80392e861855f78fc2ced7791d53620
salwa-said <salwwa2000@gmail.com>
2026-08-31 15:21:33 +0400

    SchoolService :add Query

--- src/main/java/com/cl/mysqldemo/repositories/SchoolRepository.java
+++ src/main/java/com/cl/mysqldemo/repositories/SchoolRepository.java
@@ -2,9 +2,17 @@ package com.cl.mysqldemo.repositories;
 
 import com.cl.mysqldemo.entities.School;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Long>{
+    @Query("SELECT s FROM School s WHERE s.isActive=true")
+    List<School>getAllSchool();
 
+    @Query("SELECT s FROM School s WHERE s.isActive=true AND s.id= :abc")
+    School getById(@Param("abc") Long id);
 }
