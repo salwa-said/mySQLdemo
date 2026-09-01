ba2e147e54b57ba564fd40bb16f641a3c3573ef7
salwa-said <salwwa2000@gmail.com>
2026-09-01 14:27:47 +0400

    Student.java

---
+++ src/main/java/com/cl/mysqldemo/entities/Student.java
@@ -0,0 +1,32 @@
+package com.cl.mysqldemo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String major;
+    private String gender;
+    private String phoneNumber;
+    private String parentName;
+
+    @ManyToOne
+    private School school;
+
+    private Boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
+
+
+}
