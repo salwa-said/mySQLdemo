577a6a27ab517ef9530599a7171239368435adca
salwa-said <salwwa2000@gmail.com>
2026-09-01 14:27:37 +0400

    School.java

--- src/main/java/com/cl/mysqldemo/entities/School.java
+++ src/main/java/com/cl/mysqldemo/entities/School.java
@@ -1,14 +1,12 @@
 package com.cl.mysqldemo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,6 +20,11 @@ public class School {
 
     private String name;
     private String location;
+
+
+    @OneToMany
+    List<Student> students;
+
 
     private Boolean isActive;
     private Date createdDate;
