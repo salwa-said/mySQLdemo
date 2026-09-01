111f87683dc5787c11da3f451f174c7b935b926b
salwa-said <salwwa2000@gmail.com>
2026-09-01 14:28:10 +0400

    StudentDTO.java

---
+++ src/main/java/com/cl/mysqldemo/dto/StudentDTO.java
@@ -0,0 +1,17 @@
+package com.cl.mysqldemo.dto;
+
+
+import com.cl.mysqldemo.controllers.StudentController;
+import com.cl.mysqldemo.services.StudentService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Getter
+@Setter
+public class StudentDTO {
+
+
+}
