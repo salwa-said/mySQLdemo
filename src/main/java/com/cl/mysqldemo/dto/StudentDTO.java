c9392bf918d52eeea908395e694a952400084743
salwa-said <salwwa2000@gmail.com>
2026-09-01 16:34:22 +0400

    ADD StudentDTO to StudentDTO

--- src/main/java/com/cl/mysqldemo/dto/StudentDTO.java
+++ src/main/java/com/cl/mysqldemo/dto/StudentDTO.java
@@ -1,17 +1,49 @@
 package com.cl.mysqldemo.dto;
 
-
-import com.cl.mysqldemo.controllers.StudentController;
-import com.cl.mysqldemo.services.StudentService;
+import com.cl.mysqldemo.entities.Student;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
+@Builder
+@Data
 public class StudentDTO {
 
+        private Long studentId;
+        private String studentName;
+        private String parentName;
+        private String studentMajor;
+        private String studentPhoneNumber;
+        private String gender;
 
-}
+
+        public  static StudentDTO convrToDTO(Student entity){
+            StudentDTO dto= StudentDTO.builder()
+                    .studentId(entity.getId())
+                    .studentName(entity.getName())
+                    .parentName(entity.getParentName())
+                    .studentMajor(entity.getMajor())
+                    .studentPhoneNumber(entity.getPhoneNumber())
+                    .gender(entity.getGender())
+                    .build();
+            return dto;
+        }
+
+        public  static List<StudentDTO> convrToDTO(List<Student> entityList){
+            List<StudentDTO> dtos = new ArrayList<>();
+            for (Student s: entityList){
+                dtos.add(convrToDTO(s));
+            }
+            return dtos;
+        }
+    }
+
+
+
+
