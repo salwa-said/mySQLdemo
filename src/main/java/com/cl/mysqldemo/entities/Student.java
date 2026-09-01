00261578cba2199ef188b3a17f1605d67c5e0b2e
salwa-said <salwwa2000@gmail.com>
2026-09-01 15:04:04 +0400

    delete the relation Many to one

--- src/main/java/com/cl/mysqldemo/entities/Student.java
+++ src/main/java/com/cl/mysqldemo/entities/Student.java
@@ -21,9 +21,6 @@ public class Student {
     private String phoneNumber;
     private String parentName;
 
-    @ManyToOne
-    private School school;
-
     private Boolean isActive;
     private Date createdDate;
     private Date updatedDate;
