3d531bfbbd7f93fa59cda621a314f98cae61edef
salwa-said <salwwa2000@gmail.com>
2026-09-01 14:27:55 +0400

    StudentController.java

---
+++ src/main/java/com/cl/mysqldemo/controllers/StudentController.java
@@ -0,0 +1,32 @@
+package com.cl.mysqldemo.controllers;
+
+
+import com.cl.mysqldemo.entities.Student;
+import com.cl.mysqldemo.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+
+    StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("add")
+    public Long addStudent(@RequestParam String name,
+                           @RequestParam String major,
+                           @RequestParam String gender,
+                           @RequestParam String phoneNumber,
+                           @RequestParam String parentName,
+                           @RequestParam Long schoolId){
+        return studentService.addStudent(name,major,gender,phoneNumber,parentName,schoolId);
+    }
+}
