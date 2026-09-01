6640e09efeed113b3aee4fa11c326bd39153c03c
salwa-said <salwwa2000@gmail.com>
2026-09-01 16:50:05 +0400

    ADD StudentDTO to StudentDTO

--- src/main/java/com/cl/mysqldemo/controllers/StudentController.java
+++ src/main/java/com/cl/mysqldemo/controllers/StudentController.java
@@ -1,13 +1,13 @@
 package com.cl.mysqldemo.controllers;
 
 
+import com.cl.mysqldemo.dto.StudentDTO;
 import com.cl.mysqldemo.entities.Student;
 import com.cl.mysqldemo.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("student")
@@ -31,4 +31,35 @@ public class StudentController {
     ){
         return studentService.addStudent(name, major, gender, phoneNumber, parentName, schoolId);
     }
+
+    @GetMapping("getAll")
+    public List<StudentDTO> getAllStudents() {
+        return StudentDTO.convrToDTO(studentService.getAllStudents());
+    }
+
+    @GetMapping("getById")
+    public StudentDTO getById(@RequestParam Long id) {
+        return StudentDTO.convrToDTO(studentService.getById(id));
+    }
+
+    @PutMapping("update")
+    public StudentDTO updateStudent(
+            @RequestParam Long id,
+            @RequestParam String name,
+            @RequestParam String major,
+            @RequestParam String gender,
+            @RequestParam String phoneNumber,
+            @RequestParam String parentName,
+            @RequestParam Long schoolId
+    ) {
+        return StudentDTO.convrToDTO(studentService.updateStudent(id, name, major, gender, phoneNumber, parentName, schoolId));
+    }
+
+    @DeleteMapping
+    public Boolean deleteStudent(@RequestParam Long id) {
+        return studentService.deleteById(id);
+    }
+
+
+
 }
