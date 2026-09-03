a6b6533ae1fd16cf7fc68325cce534f460442c9a
salwa-said <salwwa2000@gmail.com>
2026-09-03 15:16:11 +0400

    GlobalExceptionHandler.java

--- src/main/java/com/cl/mysqldemo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cl/mysqldemo/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,39 @@
 package com.cl.mysqldemo.exceptions;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
 
-    public Map<String, String>handelValidationException(Exception ex){
-        Map<String,String> errors= new HashMap<>();
-        errors.put(ex.getMessage(), ex.getLocalizedMessage());
-        return errors;
+@RestControllerAdvice
+
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ErrorResponse handleNotFound(ResourceNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                new Date()
+        );
+        return errorResponse;
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ErrorResponse handleGenericException(Exception ex){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                new Date()
+        );
+        return errorResponse;
     }
 
 }
